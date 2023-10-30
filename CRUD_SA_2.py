e82ccee852cebd8e3d1c596eb68b40a6256adda6
EstebanGalbán <estebangamer.eg@gmail.com>
2023-10-30 15:54:18 -0400

    Update CRUD_SA_2.py
    Funcion para verificar si existe un cliente
    Para modificar datos
    Para eliminar datos

--- CRUD_SA_2.py
+++ CRUD_SA_2.py
@@ -21,13 +21,25 @@ except mysql.connector.Error as e:
     print(f"Error al conectar a la base de datos: {e}")
     
 
+
 #######################################
 #CURD PARA LA TABLA CLIENTES
 #######################################
 
 
+# Función para verificar si un cliente existe por su cédula
+def cliente_existe_por_cedula(cedula):
+    try:
+        query = "SELECT COUNT(*) FROM Cliente WHERE Cedula_Cliente = %s"
+        cursor.execute(query, (cedula,))
+        resultado = cursor.fetchone()
+        return resultado[0] > 0
+    except mysql.connector.Error as e:
+        print(f"Error al verificar la existencia del cliente: {e}")
+        return False
+    
 
-
+    
 # Función para crear un cliente
 def crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direccion):
     try:
@@ -50,6 +62,37 @@ def crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direcc
 
 # # Llamar a la función para crear un cliente
 # crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direccion)
+
+
+
+
+# Función para modificar los datos de un cliente por su cédula
+def modificar_cliente_por_cedula(cedula, nuevo_nombre, nuevo_apellido, nuevo_telefono, nuevo_correo, nueva_direccion):
+    if cliente_existe_por_cedula(cedula):
+        try:
+            query = "UPDATE Cliente SET Nombre_Cliente = %s, Apellido_Cliente = %s, Telefono_Cliente = %s, Correo_Cliente = %s, Direccion_Cliente = %s WHERE Cedula_Cliente = %s"
+            valores = (nuevo_nombre, nuevo_apellido, nuevo_telefono, nuevo_correo, nueva_direccion, cedula)
+            cursor.execute(query, valores)
+            conexion.commit()
+            print("Datos del cliente actualizados con éxito.")
+        except mysql.connector.Error as e:
+            print(f"Error al actualizar los datos del cliente: {e}")
+    else:
+        print(f"No existe un cliente con cédula {cedula} en la base de datos.")
+
+# # Solicitar cédula del cliente al usuario desde la terminal
+# cedula = input("Introduce la cédula del cliente cuyos datos deseas modificar: ")
+
+# # Llamar a la función para modificar los datos del cliente por cédula
+# if cliente_existe_por_cedula(cedula):
+#     nuevo_nombre = input("Nuevo nombre del cliente: ")
+#     nuevo_apellido = input("Nuevo apellido del cliente: ")
+#     nuevo_telefono = input("Nuevo teléfono del cliente: ")
+#     nuevo_correo = input("Nuevo correo del cliente: ")
+#     nueva_direccion = input("Nueva dirección del cliente: ")
+#     modificar_cliente_por_cedula(cedula, nuevo_nombre, nuevo_apellido, nuevo_telefono, nuevo_correo, nueva_direccion)
+# else:
+#     print(f"No se encontró un cliente con cédula {cedula}.")
 
 
 
@@ -121,21 +164,27 @@ def consultar_cliente_por_nombre(nombre):
 
 
 
-# Función para actualizar un cliente por su ID
-def actualizar_cliente(id_cliente, nombre, apellido, telefono, correo, direccion):
-    query = "UPDATE Clientes SET Nombre_Cliente = %s, Apellido_Cliente = %s, Telefono_Cliente = %s, Correo_Cliente = %s, Direccion_Cliente = %s WHERE ID_Cliente = %s"
-    valores = (nombre, apellido, telefono, correo, direccion, id_cliente)
-    cursor.execute(query, valores)
-    conexion.commit()
-    print("Cliente actualizado con éxito.")
+# Función para eliminar un cliente por su cédula
+def eliminar_cliente_por_cedula(cedula):
+    if cliente_existe_por_cedula(cedula):
+        try:
+            query = "DELETE FROM Cliente WHERE Cedula_Cliente = %s"
+            cursor.execute(query, (cedula,))
+            conexion.commit()
+            print(f"Cliente con cédula {cedula} eliminado con éxito.")
+        except mysql.connector.Error as e:
+            print(f"Error al eliminar el cliente: {e}")
+    else:
+        print(f"No existe un cliente con cédula {cedula} en la base de datos.")
 
-# Función para eliminar un cliente por su ID
-def eliminar_cliente(id_cliente):
-    query = "DELETE FROM Clientes WHERE ID_Cliente = %s"
-    cursor.execute(query, (id_cliente,))
-    conexion.commit()
-    print("Cliente eliminado con éxito.")
-############################################
+# # Solicitar cédula del cliente al usuario desde la terminal
+# cedula = input("Introduce la cédula del cliente que deseas eliminar: ")
+
+# # Llamar a la función para eliminar el cliente por cédula
+# eliminar_cliente_por_cedula(cedula)
+
+
+
 
 #######################################
 #CURD PARA LA TABLA PROVEEDORES
