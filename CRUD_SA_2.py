e26bcba938a0de437b540bd53299a6d7f6ae959e
EstebanGalbán <estebangamer.eg@gmail.com>
2023-10-30 18:01:40 -0400

    Cambios
    Verificar si un producto existe y crear un producto

--- CRUD_SA_2.py
+++ CRUD_SA_2.py
@@ -41,10 +41,10 @@ def cliente_existe_por_cedula(cedula):
 
     
 # Función para crear un cliente
-def crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direccion):
+def crear_cliente(cedula, nombre, apellido, telefono, correo, direccion):
     try:
-        query = "INSERT INTO Cliente (ID_Cliente, Cedula_Cliente, Nombre_Cliente, Apellido_Cliente, Telefono_Cliente, Correo_Cliente, Direccion_Cliente) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        valores = (id_cliente, cedula, nombre, apellido, telefono, correo, direccion)
+        query = "INSERT INTO Cliente (Cedula_Cliente, Nombre_Cliente, Apellido_Cliente, Telefono_Cliente, Correo_Cliente, Direccion_Cliente) VALUES (%s, %s, %s, %s, %s, %s)"
+        valores = (cedula, nombre, apellido, telefono, correo, direccion)
         cursor.execute(query, valores)
         conexion.commit()
         print("Cliente creado con éxito.")
@@ -52,7 +52,6 @@ def crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direcc
         print(f"Error al crear el cliente: {e}")
 
 # # Solicitar datos al usuario desde el terminal
-# id_cliente = input("ID del cliente: ")
 # cedula = input("Cédula del cliente: ")
 # nombre = input("Nombre del cliente: ")
 # apellido = input("Apellido del cliente: ")
@@ -61,7 +60,7 @@ def crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direcc
 # direccion = input("Dirección del cliente: ")
 
 # # Llamar a la función para crear un cliente
-# crear_cliente(id_cliente, cedula, nombre, apellido, telefono, correo, direccion)
+# crear_cliente(cedula, nombre, apellido, telefono, correo, direccion)
 
 
 
@@ -208,74 +207,203 @@ def proveedor_existe_por_numero_documento(numero_documento):
 
 
 # Función para crear un proveedor
-def crear_proveedor(id_proveedor, nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion):
+def crear_proveedor(nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion):
     try:
-        query = "INSERT INTO Proveedor (ID_Proveedor, Nombre_Comercial_Proveedor, Nombre_Proveedor, Apellido_Proveedor, Tipo_Documento, Numero_Documento, Telefono_Proveedor, Correo_Proveedor, Direccion_Proveedor) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        valores = (id_proveedor, nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion)
+        query = "INSERT INTO Proveedor (Nombre_Comercial_Proveedor, Nombre_Proveedor, Apellido_Proveedor, Tipo_Documento, Numero_Documento, Telefono_Proveedor, Correo_Proveedor, Direccion_Proveedor) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s)"
+        valores = (nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion)
         cursor.execute(query, valores)
         conexion.commit()
         print("Proveedor creado con éxito.")
     except mysql.connector.Error as e:
         print(f"Error al crear el proveedor: {e}")
 
-# Solicitar datos al usuario desde el terminal para crear un proveedor
-id_proveedor = input("ID del proveedor: ")
-nombre_comercial = input("Nombre Comercial del proveedor: ")
-nombre = input("Nombre del proveedor: ")
-apellido = input("Apellido del proveedor: ")
-tipo_documento = input("Tipo de documento del proveedor: ")
-numero_documento = input("Número de documento del proveedor: ")
-telefono = input("Teléfono del proveedor: ")
-correo = input("Correo del proveedor: ")
-direccion = input("Dirección del proveedor: ")
+# # Solicitar datos al usuario desde el terminal para crear un proveedor
+# nombre_comercial = input("Nombre Comercial del proveedor: ")
+# nombre = input("Nombre del proveedor: ")
+# apellido = input("Apellido del proveedor: ")
+# tipo_documento = input("Tipo de documento del proveedor: ")
+# numero_documento = input("Número de documento del proveedor: ")
+# telefono = input("Teléfono del proveedor: ")
+# correo = input("Correo del proveedor: ")
+# direccion = input("Dirección del proveedor: ")
 
-# Llamar a la función para crear un proveedor
-crear_proveedor(id_proveedor, nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion)
+# # Llamar a la función para crear un proveedor
+# crear_proveedor( nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion)
+
+
+
+
+# Función para modificar los datos de un proveedor por su número de documento
+def modificar_proveedor_por_numero_documento(numero_documento, nuevo_nombre, nuevo_apellido, nuevo_tipo_documento, nuevo_numero_documento, nuevo_telefono, nuevo_correo, nueva_direccion):
+    if proveedor_existe_por_numero_documento(numero_documento):
+        try:
+            query = "UPDATE Proveedor SET Nombre_Proveedor = %s, Apellido_Proveedor = %s, Tipo_Documento = %s, Numero_Documento = %s, Telefono_Proveedor = %s, Correo_Proveedor = %s, Direccion_Proveedor = %s WHERE Numero_Documento = %s"
+            valores = (nuevo_nombre, nuevo_apellido, nuevo_tipo_documento, nuevo_numero_documento, nuevo_telefono, nuevo_correo, nueva_direccion, numero_documento)
+            cursor.execute(query, valores)
+            conexion.commit()
+            print("Datos del proveedor actualizados con éxito.")
+        except mysql.connector.Error as e:
+            print(f"Error al actualizar los datos del proveedor: {e}")
+    else:
+        print(f"No existe un proveedor con número de documento {numero_documento} en la base de datos.")
+
+# # Solicitar número de documento del proveedor al usuario desde la terminal
+# numero_documento = input("Introduce el número de documento del proveedor cuyos datos deseas modificar: ")
+
+# # Llamar a la función para modificar los datos del proveedor por número de documento
+# if proveedor_existe_por_numero_documento(numero_documento):
+#     nuevo_nombre = input("Nuevo nombre del proveedor: ")
+#     nuevo_apellido = input("Nuevo apellido del proveedor: ")
+#     nuevo_tipo_documento = input("Nuevo tipo de documento del proveedor: ")
+#     nuevo_numero_documento = input("Nuevo número de documento del proveedor: ")
+#     nuevo_telefono = input("Nuevo teléfono del proveedor: ")
+#     nuevo_correo = input("Nuevo correo del proveedor: ")
+#     nueva_direccion = input("Nueva dirección del proveedor: ")
+#     modificar_proveedor_por_numero_documento(numero_documento, nuevo_nombre, nuevo_apellido, nuevo_tipo_documento, nuevo_numero_documento, nuevo_telefono, nuevo_correo, nueva_direccion)
+# else:
+#     print(f"No se encontró un proveedor con número de documento {numero_documento}.")
 
 
 
 
 # Función para consultar proveedores por nombre comercial
 def consultar_proveedor_por_nombre_comercial(nombre_comercial):
-    query = "SELECT * FROM Proveedor WHERE Nombre_Comercial_Proveedor LIKE %s"
-    cursor.execute(query, (f"%{nombre_comercial}%",))
-    resultado = cursor.fetchall()
-    return resultado
+    try:
+        query = "SELECT * FROM Proveedor WHERE Nombre_Comercial_Proveedor LIKE %s"
+        cursor.execute(query, (f"%{nombre_comercial}%",))
+        resultado = cursor.fetchall()
+        return resultado
+    except mysql.connector.Error as e:
+        print(f"Error al consultar proveedor por nombre comercial: {e}")
+        return None
+
+# # Solicitar nombre comercial del proveedor al usuario desde la terminal
+# nombre_comercial = input("Introduce el nombre comercial del proveedor a consultar: ")
+
+# # Llamar a la función para consultar el proveedor por nombre comercial
+# resultado = consultar_proveedor_por_nombre_comercial(nombre_comercial)
+
+# if resultado:
+#     print("Proveedor encontrado:")
+#     for row in resultado:
+#         print(f"ID: {row[0]}")
+#         print(f"Nombre Comercial: {row[1]}")
+#         print(f"Nombre: {row[2]}")
+#         print(f"Apellido: {row[3]}")
+#         print(f"Tipo de Documento: {row[4]}")
+#         print(f"Número de Documento: {row[5]}")
+#         print(f"Teléfono: {row[6]}")
+#         print(f"Correo: {row[7]}")
+#         print(f"Dirección: {row[8]}")
+# else:
+#     print("Proveedor no encontrado.")
+
+
 
 # Función para consultar proveedores por número de documento
 def consultar_proveedor_por_numero_documento(numero_documento):
-    query = "SELECT * FROM Proveedor WHERE Numero_Documento = %s"
-    cursor.execute(query, (numero_documento,))
-    resultado = cursor.fetchall()
-    return resultado
+    try:
+        query = "SELECT * FROM Proveedor WHERE Numero_Documento = %s"
+        cursor.execute(query, (numero_documento,))
+        resultado = cursor.fetchall()
+        return resultado
+    except mysql.connector.Error as e:
+        print(f"Error al consultar proveedor por número de documento: {e}")
+        return None
 
-# Función para actualizar un proveedor por su ID
-def actualizar_proveedor(id_proveedor, nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion):
-    query = "UPDATE Proveedor SET Nombre_Comercial_Proveedor = %s, Nombre_Proveedor = %s, Apellido_Proveedor = %s, Tipo_Documento = %s, Numero_Documento = %s, Telefono_Proveedor = %s, Correo_Proveedor = %s, Direccion_Proveedor = %s WHERE ID_Proveedor = %s"
-    valores = (nombre_comercial, nombre, apellido, tipo_documento, numero_documento, telefono, correo, direccion, id_proveedor)
-    cursor.execute(query, valores)
-    conexion.commit()
-    print("Proveedor actualizado con éxito.")
+# # Solicitar número de documento del proveedor al usuario desde la terminal
+# numero_documento = input("Introduce el número de documento del proveedor a consultar: ")
 
-# Función para eliminar un proveedor por su ID
-def eliminar_proveedor(id_proveedor):
-    query = "DELETE FROM Proveedor WHERE ID_Proveedor = %s"
-    cursor.execute(query, (id_proveedor,))
-    conexion.commit()
-    print("Proveedor eliminado con éxito.")
+# # Llamar a la función para consultar el proveedor por número de documento
+# resultado = consultar_proveedor_por_numero_documento(numero_documento)
+
+# if resultado:
+#     print("Proveedor encontrado:")
+#     for row in resultado:
+#         print(f"ID: {row[0]}")
+#         print(f"Nombre Comercial: {row[1]}")
+#         print(f"Nombre: {row[2]}")
+#         print(f"Apellido: {row[3]}")
+#         print(f"Tipo de Documento: {row[4]}")
+#         print(f"Número de Documento: {row[5]}")
+#         print(f"Teléfono: {row[6]}")
+#         print(f"Correo: {row[7]}")
+#         print(f"Dirección: {row[8]}")
+# else:
+#     print("Proveedor no encontrado.")
+
+
+
+# Función para eliminar un proveedor por su número de documento
+def eliminar_proveedor_por_numero_documento(numero_documento):
+    if proveedor_existe_por_numero_documento(numero_documento):
+        try:
+            query = "DELETE FROM Proveedor WHERE Numero_Documento = %s"
+            cursor.execute(query, (numero_documento,))
+            conexion.commit()
+            print(f"Proveedor con número de documento {numero_documento} eliminado con éxito.")
+        except mysql.connector.Error as e:
+            print(f"Error al eliminar el proveedor: {e}")
+    else:
+        print(f"No existe un proveedor con número de documento {numero_documento} en la base de datos.")
+
+# # Solicitar número de documento del proveedor al usuario desde la terminal
+# numero_documento = input("Introduce el número de documento del proveedor que deseas eliminar: ")
+
+# # Llamar a la función para eliminar el proveedor por número de documento
+# eliminar_proveedor_por_numero_documento(numero_documento)
+
+
 
 
 #######################################
 #CURD PARA LA TABLA PRODUCTOS
 #######################################
-#
+
+
+
+
+# Función para verificar si un producto existe por su código
+def producto_existe_por_codigo(codigo):
+    try:
+        query = "SELECT COUNT(*) FROM Productos WHERE Codigo_Producto = %s"
+        cursor.execute(query, (codigo,))
+        resultado = cursor.fetchone()
+        return resultado[0] > 0
+    except mysql.connector.Error as e:
+        print(f"Error al verificar la existencia del producto: {e}")
+        return False
+
+
+
 # Función para crear un producto
-def crear_producto(nombre, codigo, detalle, cantidad, precio_venta, precio_costo, fecha_ingreso, descuento, promocion):
-    query = "INSERT INTO Productos (Nombre_Producto, Codigo_Producto, Detalle_Producto, Cantidad_Producto, Precio_Venta, Precio_Costo, Fecha_Ingreso, Descuento, Promocion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    valores = (nombre, codigo, detalle, cantidad, precio_venta, precio_costo, fecha_ingreso, descuento, promocion)
-    cursor.execute(query, valores)
-    conexion.commit()
-    print("Producto creado con éxito.")
+def crear_producto(nombre, codigo, detalle, cantidad, precio_venta, precio_costo, fecha_ingreso, id_proveedor):
+    try:
+        query = "INSERT INTO Productos (Nombre_Producto, Codigo_Producto, Detalle_Producto, Cantidad_Producto, Precio_Venta, Precio_Costo, Fecha_Ingreso, Proveedor_ID_Proveedor) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        valores = (nombre, codigo, detalle, cantidad, precio_venta, precio_costo, fecha_ingreso, id_proveedor)
+        cursor.execute(query, valores)
+        conexion.commit()
+        print("Producto creado con éxito.")
+    except mysql.connector.Error as e:
+        print(f"Error al crear el producto: {e}")
+
+# Solicitar al usuario el ID del proveedor al que desea asociar el producto
+id_proveedor = input("Ingrese el ID del proveedor al que desea asociar el producto: ")
+
+# # Solicitar datos al usuario desde el terminal para crear un producto
+# nombre = input("Nombre del producto: ")
+# codigo = input("Código del producto: ")
+# detalle = input("Detalle del producto: ")
+# cantidad = input("Cantidad del producto: ")
+# precio_venta = input("Precio de venta del producto: ")
+# precio_costo = input("Precio de costo del producto: ")
+# fecha_ingreso = input("Fecha de ingreso del producto(Example:2023-10-20): ")
+
+# # Llamar a la función para crear un producto, proporcionando el ID del proveedor
+# crear_producto(nombre, codigo, detalle, cantidad, precio_venta, precio_costo, fecha_ingreso, id_proveedor)
+
+
+
 
 # Función para consultar productos por nombre
 def consultar_producto_por_nombre(nombre):
